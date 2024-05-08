425a1ce3bfe804a31bc6c4155d14319e4887455f
Alamin-eng <alaminruben@gmail.com>
2024-05-08 02:46:37 +0100

    icons added

--- src/header/header.js
+++ src/header/header.js
@@ -1,5 +1,6 @@
 // import gif from '../../public/video-drama.gif';
 import gif from "../assets/video-drama.gif";
+import user from "../assets/account_circle_.svg";
 import "./header.css";
 
 export default function Header() {
@@ -9,13 +10,33 @@ export default function Header() {
         <img className="header-gif" src={gif} alt="animated gif" />
       </div>
       <div>
-      <h1>Vidoes library</h1>
+        <h1>Vidoes library</h1>
       </div>
       <div className="header-search">
         <div className="search">search</div>
       </div>
-      <div className="header-addbuttons">add buttons</div>
-      <div>users</div>
+      <div className="header-addbuttons">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="34px"
+          viewBox="0 -960 960 960"
+          width="34px"
+          fill="#BB271A"
+        >
+          <path d="M616-242q-27 1-51.5 1.5t-43.5.5h-41q-71 0-133-2-53-2-104.5-5.5T168-257q-26-7-45-26t-26-45q-6-23-9.5-56T82-447q-2-36-2-73t2-73q2-30 5.5-63t9.5-56q7-26 26-45t45-26q23-6 74.5-9.5T347-798q62-2 133-2t133 2q53 2 104.5 5.5T792-783q26 7 45 26t26 45q6 23 9.5 56t5.5 63q2 36 2 73v17q-19-8-39-12.5t-41-4.5q-83 0-141.5 58.5T600-320q0 21 4 40.5t12 37.5ZM400-400l208-120-208-120v240Zm360 200v-80h-80v-80h80v-80h80v80h80v80h-80v80h-80Z" />
+        </svg>
+      </div>
+      <div className="header-user">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          fill="#D08370"
+        >
+          <path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z" />
+        </svg>
+      </div>
     </div>
   );
 }
