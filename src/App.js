a0643ae3c9ddeaca06aea998aced4bf6b8aa00ec
Alamin-eng <alaminruben@gmail.com>
2024-05-17 00:25:19 +0100

    banner added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Header from './header/header';
+import Banner from './banner/banner';
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <Banner />
     </div>
   );
 }
---
+++ src/banner/banner.js
@@ -0,0 +1,14 @@
+
+import "./banner.css";
+
+export default function Banner() {
+    return (
+      <div className="banner">
+        <div class="banner-text">
+          <h1>Welcome, user</h1>
+          <p>In this site you will be able to create your youtube vidoes library. The site is user friendly which allows you to add, remove or rate your favourite youtube vidoe's of your choice.</p>
+          <button>Add videos</button>
+        </div>
+      </div>
+    );
+}
