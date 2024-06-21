9367f240915e04b5aae530ceee04e6c115eee0a8
Alamin-eng <alaminruben@gmail.com>
2024-06-21 03:32:31 +0100

    github actions

--- src/banner/banner.js
+++ src/banner/banner.js
@@ -4,7 +4,7 @@ import "./banner.css";
 export default function Banner() {
     return (
       <div className="banner">
-        <div class="banner-text">
+        <div class="banner-text" style={{color:"yellow"}}>
           <h1>Welcome, user</h1>
           <p>In this site you will be able to create your youtube vidoes library. The site is user friendly which allows you to add, remove or rate your favourite youtube vidoe's of your choice.</p>
           <button>Add videos</button>
